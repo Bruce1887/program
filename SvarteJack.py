6913a8550b3bdda3909d69bf15cf199bb8ef8c3f
Edvin Bruce <edbr8688@gullstrand.it.uu.se>
2021-10-05 10:01:23 +0200

    Första

---
+++ SvarteJack.py
@@ -0,0 +1,16 @@
+def valör():
+    for n in range(13):     
+        return(n+1)
+
+def färg():
+    s = ["Hjärter","Ruter","Spader","Klöver"]
+    return(s)
+
+def kort():
+    s = []
+    for n in range(4):
+        s.färg()[n].append(valör)
+    return(s)
+
+print(kort())
+
